8efc207c5d82bd38bb36d813346f28e7734be61c
keyminy <imanaifs@gmail.com>
2024-04-26 15:30:24 +0900

    초기

---
+++ 04.TIMER_TEST/main.c
@@ -0,0 +1,21 @@
+/*
+ * 04.TIMER_TEST.c
+ *
+ * Created: 2024-04-26 오후 3:23:34
+ * Author : HARMAN-27
+ */ 
+
+#include <avr/io.h>
+#include F_CPU 16000000UL
+#include <util/delay.h>
+//interrupt 관련 함수
+#include <avr/interrupt.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
