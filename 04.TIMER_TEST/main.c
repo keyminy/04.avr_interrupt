25d39b2a29adb470353352b9ba8bbd2bf4dd9299
keyminy <imanaifs@gmail.com>
2024-04-26 16:01:27 +0900

    인터럽트 0426금

--- 04.TIMER_TEST/main.c
+++ 04.TIMER_TEST/main.c
@@ -16,6 +16,28 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
+		
     }
 }
 
+//timer0를 초기화 한다.
+void init_timer0(){
+	//8bit timer, timer 0번과 2번 사용한다.
+	//분주비를 64로 설정한다.
+	// 1.분주비 계산
+	// 16000000Hz/64 = 250,000Hz
+	
+	// 2.T(주기) : pulse 1개가 잡아 먹는 시간(주파수의 역수 : 1/f)
+	// 1 / 250,000 = 0.000004sec(=4us,0.004ms)
+	// 3. 8bit timer overflow : 0.004ms * 256개 카운트 = 0.001024sec = 1.024ms
+	// 1ms가 정확히 안되자너... 1ms가 정확히 될려면 어떻게 해야하나?
+	// 0.004ms * 250 = 0.001(=1ms)
+	
+	//진짜 나중에 추가한 코드임
+	TCNT0 = 6; // 6부터 count하는것임, TCNT : 0~256카운트하기때문에, 정확히 1ms마다 TIMER0_OVF_vect로 진입한다.
+	
+	TCCR0 |= (1 << CS02) | (0 << CS01) | (0 << CS00); // (1)분주비 64로 셋팅(p.296 표13-1 참고)
+	TIMSK = 1 << TOIE0; // (2) TIMER0 overflow interrupt허용
+	sei(); // 전역적으로 인터럽트를 허용하겠다
+	
+}
